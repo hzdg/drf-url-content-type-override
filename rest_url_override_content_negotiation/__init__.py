7f30e283e54475d2b17f1ff29585220be2b75717
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-10-07 17:51:06 -0400

    Wrap parsers in order to send overridden content type

--- rest_url_override_content_negotiation/__init__.py
+++ rest_url_override_content_negotiation/__init__.py
@@ -10,5 +10,14 @@ class URLOverrideContentNegotiation(DefaultContentNegotiation):
                                                     content_type)
         for parser in parsers:
             if media_type_matches(parser.media_type, content_type):
-                return parser
+
+                # Create a parser that proxies to the matching one but with the
+                # overridden content type.
+                class ParserWrapper(object):
+                    media_type = content_type
+
+                    def parse(self, stream, media_type=None, parser_context=None):
+                        return parser.parse(stream, content_type, parser_context)
+
+                return ParserWrapper()
         return None
