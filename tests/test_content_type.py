86036942f32b629e7d3ccc5307be6b3e03ae4053
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-10-07 16:42:17 -0400

    Test that not all content types are overridden

--- tests/test_content_type.py
+++ tests/test_content_type.py
@@ -26,3 +26,21 @@ def test_content_type_override_query():
                      content_type='text/plain'))
     assert None is negotiation.select_parser(
         requestWithoutQueryParam, parsers)
+
+
+def test_limited_overrides():
+    """
+    The content type shouldn't be overridden if the header is something other
+    than 'text/plain', or missing entirely.
+
+    """
+    from rest_url_override_content_negotiation import \
+        URLOverrideContentNegotiation
+    negotiation = URLOverrideContentNegotiation()
+    parsers = (JSONParser, FormParser, MultiPartParser)
+
+    req = Request(
+        factory.post('/?content_type=application/x-www-form-urlencoded',
+                     {'email': 'mmmmmm@test.com'},
+                     content_type='text/somethingelse'))
+    assert negotiation.select_parser(req, parsers) is None
